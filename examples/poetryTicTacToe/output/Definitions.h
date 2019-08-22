181f7c3ba6f431cda7a22fbc12c66e25851b6571
conor <clmurphy@sfu.ca>
2019-08-21 23:50:29 -0400

    added examples

---
+++ examples/poetryTicTacToe/output/Definitions.h
@@ -0,0 +1,109 @@
+/* preprocessor functions found in source file */
+#include <iostream>
+
+/* Define all the words in the text file */
+#define Two using namespace
+#define roads std; char square[10] =
+#define diverged_in { 'o' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8'
+#define a_yellow , '9' }; int checkwin();
+#define wood void board(); int
+#define And_sorry main() { int player = 1,i,choice; char mark; do { board();
+#define I_could_not_travel player=(player%2)?1:2;
+#define both cout << "Player " <<
+#define And_be player << ", enter a number:  " ; cin
+#define one_traveler >> choice; mark=(player
+#define long_I == 1) ? 'X' : 'O' ;
+#define stood if (choice == 1 &&
+#define And_looked square[1] == '1' ) square[1]
+#define down = mark; else
+#define one if (choice == 2 &&
+#define as_far square[2] == '2' ) square[2]
+#define as_I = mark; else
+#define could if (choice == 3 &&
+#define To square[3] == '3' ) square[3]
+#define where = mark; else
+#define it_bent if (choice == 4 &&
+#define in_the square[4] == '4' ) square[4]
+#define undergrowth = mark; else
+#define Then if (choice == 5 &&
+#define took square[5] == '5' ) square[5]
+#define the_other = mark; else
+#define as if (choice == 6 &&
+#define just square[6] == '6' ) square[6]
+#define as_fair = mark; else
+#define And_having if (choice == 7 &&
+#define perhaps square[7] == '7' ) square[7]
+#define the_better = mark; else
+#define claim if (choice == 8 &&
+#define Because square[8] == '8' ) square[8]
+#define it = mark; else
+#define was if (choice == 9 &&
+#define grassy square[9] == '9' ) square[9]
+#define and_wanted = mark; else
+#define wear { cout<< "Invalid move " ;
+#define Though player--; cin.ignore();
+#define as_for_that cin.get(); }
+#define the_passing i=checkwin(); player++;
+#define there }while(i==-1);
+#define Had board(); if(i==1)
+#define worn cout<< "==>\aPlayer "
+#define them <<--player<<
+#define really " win " ; else cout<<
+#define about "==>\aGame draw" ;
+#define the_same cin.ignore(); cin.get();
+#define And_both_that return 0; } int
+#define morning checkwin() {
+#define equally if (square[1] ==
+#define lay square[2] && square[2] ==
+#define In square[3]) return 1; else
+#define leaves if (square[4] ==
+#define no square[5] && square[5] ==
+#define step square[6]) return 1; else
+#define had if (square[7] ==
+#define trodden square[8] && square[8] ==
+#define black square[9]) return 1; else
+#define Oh if (square[1] ==
+#define I_kept square[4] && square[4] ==
+#define the_first square[7]) return 1; else
+#define for_another if (square[2] ==
+#define day square[5] && square[5] ==
+#define Yet square[8]) return 1; else
+#define knowing if (square[3] ==
+#define how square[6] && square[6] ==
+#define way_leads square[9]) return 1; else
+#define on if (square[1] ==
+#define to square[5] && square[5] ==
+#define way square[9]) return 1; else
+#define I_doubted if (square[3] ==
+#define if_I square[5] && square[5] ==
+#define should square[7]) return 1; else
+#define ever if (square[1] != '1' &&
+#define come square[2] != '2' &&
+#define back square[3] != '3' &&
+#define I square[4] != '4' &&
+#define shall square[5] != '5' &&
+#define be square[6] != '6' &&
+#define telling square[7] != '7' &&
+#define this_with square[8] != '8' &&
+#define a_sigh square[9] != '9' ) return 0; else
+#define Somewhere return -1; } void board() {
+#define ages system( "cls" ); cout
+#define and_ages << "\n\n\tTic Tac Toe\n\n" ;
+#define hence cout << "Player 1 (X)  -  Player 2 (O)" << endl <<
+#define Two_roads endl; cout <<
+#define diverged endl; cout << "     |     |     " <<
+#define in endl; cout << "  " <<
+#define a_wood square[1] << "  |  " <<
+#define and_I square[2] << "  |  " <<
+#define I_took square[3] << endl; cout << "_____|_____|_____" <<
+#define the endl; cout << "     |     |     " <<
+#define one_less endl; cout << "  " <<
+#define traveled square[4] << "  |  " <<
+#define by square[5] << "  |  " <<
+#define And square[6] << endl; cout << "_____|_____|_____" <<
+#define that endl; cout << "     |     |     " <<
+#define has endl; cout << "  " <<
+#define made square[7] << "  |  " <<
+#define all square[8] << "  |  " <<
+#define the_difference square[9] << endl; cout << "     |     |     " << endl << endl; }
+
---
+++ examples/poetryTicTacToe/output/beautiful_ticTacToe.cpp
@@ -0,0 +1,23 @@
+/* Include Definitions */
+#include "Definitions.h"
+
+Two roads diverged_in a_yellow wood
+And_sorry I_could_not_travel both
+And_be one_traveler long_I stood
+And_looked down one as_far as_I could
+To where it_bent in_the undergrowth
+Then took the_other as just as_fair
+And_having perhaps the_better claim
+Because it was grassy and_wanted wear
+Though as_for_that the_passing there
+Had worn them really about the_same
+And_both_that morning equally lay
+In leaves no step had trodden black
+Oh I_kept the_first for_another day
+Yet knowing how way_leads on to way
+I_doubted if_I should ever come back
+I shall be telling this_with a_sigh
+Somewhere ages and_ages hence
+Two_roads diverged in a_wood and_I
+I_took the one_less traveled by
+And that has made all the_difference
